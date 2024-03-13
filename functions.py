fbe72759160768b3587614b1c2a41c5d9e8c2a63
rfi-cfif <rfi-cfif1@mail.ru>
2024-03-13 20:03:55 +0300

    12

---
+++ functions.py
@@ -0,0 +1,28 @@
+import sympy as sym
+import numpy as np
+
+
+def grad(f, var, A):
+    a = []
+    podst = list(zip(var, A))
+    for x in var:
+        a.append(sym.diff(f, x, 1).subs(podst))
+    return np.array(a)
+
+
+def grad_fall(f, var, A, k):
+    gf = grad(f, var, A)
+    A = np.array(A)
+    A = A - k * gf
+    return A
+
+
+x = sym.Symbol('x')
+y = sym.Symbol('y')
+f = 13*x**3-7*x*y+y**2+2*y**3-97
+A = (1, 2)
+k = 0.1
+A1 = grad_fall(f, (x, y), A=A, k=k)
+gf = grad(f, (x, y), A1)
+A2 = grad_fall(f, (x, y), A=A1, k=k)
+print(A2)
